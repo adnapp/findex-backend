3bfe0bcd5385dbf1a0b78db9e55a492175b7f6ab
adnapp <adam.napps@gmail.com>
2021-03-30 21:46:20 -0400

    added crypto hidden api

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,3 +1,9 @@
+require 'coinbase/wallet'
+require_relative '../../.api_key.rb'
+CLIENT = Coinbase::Wallet::Client.new(api_key: $api_key, api_secret: $api_secret )
+
+
+
 class TransactionsController < ApplicationController
 
     def index 
@@ -19,6 +25,21 @@ class TransactionsController < ApplicationController
         transaction = Transaction.find(params[:id])
         transaction.destroy
         render json: transaction
+    end
+
+    def testCrypto 
+
+        account_id ="e0172921-57bc-53e5-aa6c-d6a02ea5d1ee"
+        btcAdd1 = "7ec66567-2239-571b-8b45-234e70da9bcb"
+        btcAdd2 = "652da520-9e4c-5ee6-be2e-f7c78c946ad2"
+
+        account = CLIENT.account(account_id)
+        transactions = account.list_buys
+        addresses = account.address(btcAdd1)
+        
+        render json: transactions
+ 
+
     end
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :monthly_budgets
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/cryptoTransactions', to: 'transactions#testCrypto'
+
 end
